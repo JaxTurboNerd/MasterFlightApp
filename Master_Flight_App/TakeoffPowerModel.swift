3e7abc022ccd31215839ba893e70c26c9d6bcbd8
Gregory Boyd <gboyd69@gregorys-macbook-air.local>
2017-08-28 18:10:47 -0400

    Initial Commit

---
+++ Master_Flight_App/TakeoffPowerModel.swift
@@ -0,0 +1,60 @@
+//
+//  TakeoffPowerModel.swift
+//  Master_Flight_App
+//
+//  Created by Gregory Boyd on 8/18/17.
+//  Copyright © 2017 Gregory Boyd. All rights reserved.
+//
+
+import Foundation
+
+struct TakeoffPower {
+    
+    //SHP array
+    let SHP1077List  = [4600, 4560, 4530, 4490, 4450, 4420, 4390, 4360, 4320, 4290, 4260, 4220, 4185, 4150, 4120, 4080, 4040,
+                        4010, 3970, 3940, 3910, 3870, 3840, 3810, 3770, 3740, 3700, 3660, 3630]
+    
+    let SHP1010List = [4600, 4560, 4525, 4500, 4465, 4435, 4400, 4380, 4355, 4320, 4290, 4260, 4220, 4195, 4160, 4130, 4100, 4080, 4040,
+                       4010, 3980, 3950, 3910, 3880, 3850, 3820, 3790, 3760, 3730, 3700, 3665, 3640, 3610, 3580, 3545, 3510, 3480, 3440,
+                       3410, 3380, 3360, 3330, 3300, 3260, 3230, 3200, 3160, 3130]
+    
+    mutating func calculatePower(airTemp: Int, antiIce: Bool, pressureAltitude: Int) -> (stringSHP100:String, stringSHP95: String) {
+        
+        var SHP100:Int = 0
+        var SHP95: Double = 0.0
+        
+        
+        //Adjusts for use of Engine Anti-ice
+        var workingOAT = airTemp
+        if antiIce == true {
+            workingOAT = workingOAT + 13
+        }
+    
+        //Limits the value of TempInput to 45 degrees
+        if workingOAT > 45 {
+            workingOAT = 45
+        }
+
+        if workingOAT < 17 {
+            workingOAT = 17
+        }
+    
+        // Pressure Altitude correction
+         if pressureAltitude  >= 100 {
+           SHP100 = SHP1077List[workingOAT - 17] - (12*(pressureAltitude/100))
+           SHP95 = Double(SHP100) * 0.95
+         }
+         else {  //Calculated 100% & 95% SHP with no Pressure Altitude corrections
+
+            SHP100 = SHP1077List[workingOAT - 17]
+            SHP95 = Double(SHP100) * 0.95
+        }
+        
+        //Convert final SHP to type String
+        let stringSHP100 = String(SHP100)
+        let stringSHP95 = String(Int(SHP95))
+        
+        return (stringSHP100, stringSHP95)
+    }
+    
+}
---
+++ Master_Flight_App/TakeoffPowerViewController.swift
@@ -0,0 +1,130 @@
+//
+//  TakeoffPowerViewController.swift
+//  Master_Flight_App
+//
+//  Created by Gregory Boyd on 8/11/17.
+//  Copyright © 2017 Gregory Boyd. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet weak var TITlabel: UILabel!
+    
+    //TIT Options entered by user
+    @IBOutlet weak var TIT_Picker: UIPickerView!
+    var TIT_PickerData: [String] = [String]()
+    
+    //Set Engine Anti Ice condition
+    @IBOutlet weak var AntiIceLabel: UILabel!    
+    var isAntiIceOn: Bool?
+    
+    @IBAction func engineAntiIce(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            AntiIceLabel.text = "On"
+        }
+        else {
+            AntiIceLabel.text = "Off"
+        }
+    }
+    
+    @IBOutlet weak var SHP95: UILabel!
+    @IBOutlet weak var SHP100: UILabel!
+    @IBOutlet weak var OAT: UITextField!
+    @IBOutlet weak var PA: UITextField!
+
+    @IBAction func calculatePower(_ sender: UIButton) {
+        //MVC code
+        var powerCalculation = TakeoffPower()
+        
+        if AntiIceLabel.text == "On" {
+            isAntiIceOn = true
+        }
+        else {
+            isAntiIceOn = false
+        }
+        //assign values to label
+        SHP100.text = powerCalculation.calculatePower(airTemp: Int(OAT.text!)!, antiIce: isAntiIceOn!, pressureAltitude: Int(PA.text!)!).stringSHP100
+        SHP95.text = powerCalculation.calculatePower(airTemp: Int(OAT.text!)!, antiIce: isAntiIceOn!, pressureAltitude: Int(PA.text!)!).stringSHP95
+        //dismiss keyboard after click calculate button
+        self.doneClicked()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Connect Data:
+        self.TIT_Picker.delegate = self
+        self.TIT_Picker.dataSource = self
+        
+        //TIT picker data
+        TIT_PickerData = ["1077", "1010"]
+        
+        //Add Done Button to top of keypad
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        
+        //pushes the done button to the right side of the toolbar
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        
+        //adds done button to a toolbar above the keypad
+        let doneButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action: #selector(self.doneClicked))
+        
+        let minusButton = UIBarButtonItem(title: "-",style: .plain, target: self, action: #selector(toggleMinus))
+        
+        toolBar.setItems([minusButton, flexibleSpace, doneButton], animated: false)
+        
+        OAT.inputAccessoryView = toolBar
+        
+        //dismiss keyboard by tapping anywhere
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target:
+            self.view, action: #selector(UIView.endEditing(_:))))
+
+    }
+    
+    func doneClicked() {
+        view.endEditing(true)
+    }
+    
+    func toggleMinus(){
+        if var text = OAT.text, text.isEmpty == false {
+            if text.hasPrefix("-") {
+                text.replacingOccurrences(of: "-", with: "")
+            }
+            else {
+                text = "-\(text)"
+            }
+            
+            OAT.text = text
+        }
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+
+    }
+
+    //MARK: Data Sources
+    
+    func numberOfComponents(in pickerView: UIPickerView)-> Int {
+        return 1
+    }
+
+    func pickerView(_: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return TIT_PickerData.count
+    }
+    
+    //MARK: Delegates
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return TIT_PickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        TITlabel.text = TIT_PickerData[row]
+    }
+    
+    // Dispose of any resources that can be recreated.
+}
+
