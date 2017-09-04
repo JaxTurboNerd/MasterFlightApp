0c12c4b4c0661351272e97a84bfc447cd62132d7
Gregory Boyd <gboyd69@gregorys-macbook-air.local>
2017-09-04 12:34:32 -0400

    Added Mission Tab

---
+++ MissionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MissionViewController.swift
+//  Master_Flight_App
+//
+//  Created by Gregory Boyd on 9/4/17.
+//  Copyright © 2017 Gregory Boyd. All rights reserved.
+//
+
+import UIKit
+
+class MissionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
