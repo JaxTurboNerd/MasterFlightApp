0b8a31b150cce9825bb0edb9c0f53c4c423db252
Gregory Boyd <gboyd69@gregorys-macbook-air.local>
2017-09-16 20:16:20 -0400

    added code to on station model

--- Master_Flight_App/OnStationModel.swift
+++ Master_Flight_App/OnStationModel.swift
@@ -11,15 +11,20 @@ import Foundation
 
 struct OnStationMode {
     
-    func calculateOnSta(totalFuel: Double, onStationFuelFlow: Int, returnFuelFlow: Int, destDistance: Int, groundSpeed: Int, onTopFuel: Double ) -> (onStationTime: Double, goHomeFuel: Double) {
+    func calculateOnSta(totalFuel: Double, onStationFuelFlow: Int, returnFuelFlow: Int, destDistance: Int, groundSpeed: Int, onTopFuel: Double ) -> (stringOnStationTime: String, stringFlyHomeFuel: String) {
         
         /*
          Need to convert all input number to thousands befor the math calculations
          */
         //code for calculations
-        let flyHomeFuel = (destDistance/groundSpeed) * returnFuelFlow
-        let onStationFuel = totalFuel - onTopFuel - flyHomeFuel
-        let onStationTime = totalFuel/onStationFuelFlow
+        let flyHomeFuel = Double((destDistance/groundSpeed) * returnFuelFlow)
+        let onStationFuel = totalFuel - onTopFuel - Double(flyHomeFuel)
+        let onStationTime = onStationFuel/Double(onStationFuelFlow)
         
+        //convert to type String for labels
+        let stringOnStationTime = String(onStationTime)
+        let stringFlyHomeFuel = String(flyHomeFuel)
+        
+        return(stringOnStationTime, stringFlyHomeFuel)
     }
 }
--- Master_Flight_App/OnstationViewController.swift
+++ Master_Flight_App/OnstationViewController.swift
@@ -11,22 +11,55 @@ import UIKit
 class OnstationViewController: UIViewController {
 
     @IBOutlet weak var TotalFuel: UITextField!
-   
+    @IBOutlet weak var onstaFuelFLow: UITextField!
+    @IBOutlet weak var returnFuelFlow: UITextField!
     @IBOutlet weak var DestDistance: UITextField!
     @IBOutlet weak var GroundSpeed: UITextField!
     @IBOutlet weak var OnTop: UITextField!
+    
     @IBOutlet weak var OnStationTime: UILabel!
+    @IBOutlet weak var goHomeFuel: UILabel!
     
     
     @IBAction func calcOnStation(_ sender: UIButton) {
+        let OnStationCalculation = OnStationMode()
+        
+        OnStationTime.text = OnStationCalculation.calculateOnSta(totalFuel: Double(TotalFuel.text!)!, onStationFuelFlow: Int(onstaFuelFLow.text!)!, returnFuelFlow: Int(returnFuelFlow.text!)!, destDistance: Int(DestDistance.text!)!, groundSpeed: Int(GroundSpeed.text!)!, onTopFuel: Double(OnTop.text!)!).stringOnStationTime
+        
+        goHomeFuel.text = OnStationCalculation.calculateOnSta(totalFuel: Double(TotalFuel.text!)!, onStationFuelFlow: Int(onstaFuelFLow.text!)!, returnFuelFlow: Int(returnFuelFlow.text!)!, destDistance: Int(DestDistance.text!)!, groundSpeed: Int(GroundSpeed.text!)!, onTopFuel: Double(OnTop.text!)!).stringFlyHomeFuel
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //Add Done Button to top of keypad
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        
+        //pushes the done button to the right side of the toolbar
+        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
+        
+        //adds done button to a toolbar above the keypad
+        let doneButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action: #selector(self.doneClicked))
+        
+        toolBar.setItems([flexibleSpace, doneButton], animated: false)
+        
+        TotalFuel.inputAccessoryView = toolBar
+        onstaFuelFLow.inputAccessoryView = toolBar
+        returnFuelFlow.inputAccessoryView = toolBar
+        DestDistance.inputAccessoryView = toolBar
+        GroundSpeed.inputAccessoryView = toolBar
+        OnTop.inputAccessoryView = toolBar
+        
+        //dismiss keyboard by tapping anywhere
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target:
+            self.view, action: #selector(UIView.endEditing(_:))))
+
     }
 
+    func doneClicked() {
+        view.endEditing(true)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
