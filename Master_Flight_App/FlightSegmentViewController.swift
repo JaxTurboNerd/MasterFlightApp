51fab3cacf41ddcfcf4a1bbb74343aac18e80559
Gregory Boyd <gboyd69@gregorys-macbook-air.local>
2017-10-05 13:06:59 -0400

    added two new view controllers.  Segment times and GLO report

---
+++ Master_Flight_App/FlightSegmentViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FlightSegmentViewController.swift
+//  Master_Flight_App
+//
+//  Created by Gregory Boyd on 10/5/17.
+//  Copyright © 2017 Gregory Boyd. All rights reserved.
+//
+
+import UIKit
+
+class FlightSegmentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
